6c3fa5bb3080ae327a2249ff58f03a5e6a739692
doshgo <34873349+doshgo@users.noreply.github.com>
2024-03-11 13:49:05 +0000

    Added additional formatting to HTML and CSS files

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ def weather():
   temp_kelvin = response['main']['temp']
   temp_celsius = kelvin_to_celsius(temp_kelvin)
   temp_farenheit = kelvin_to_farenheit(temp_celsius)
+  description = response['weather']['main']
+  print(response);
   return render_template('weatherReport.html', 
       city = city, 
-      celsius = round(temp_celsius, 2), farenheit = round(temp_farenheit, 2))
+      celsius = round(temp_celsius, 2), farenheit = round(temp_farenheit, 2), description = description)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
